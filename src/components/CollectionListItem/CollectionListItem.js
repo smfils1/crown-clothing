c8111d174dfb87f01bb06b296171d1d72583a6e6
Samuel Fils <smfils1@gmail.com>
2020-01-11 14:26:39 -0500

    Added hover effect to collectionlistitems

--- src/components/CollectionListItem/CollectionListItem.js
+++ src/components/CollectionListItem/CollectionListItem.js
@@ -2,11 +2,15 @@ import React from "react";
 
 import "./CollectionListItem.scss";
 
-const CollectionListItem = ({title, imageUrl, size}) => {
+const CollectionListItem = ({ title, imageUrl, size }) => {
     return (
-        <div style= {{
-            backgroundImage: `url(${imageUrl})`
-        }} className={`${size} menu-item`}>
+        <div className={`${size} menu-item`}>
+            <div
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}
+                className="background-image"
+            ></div>
             <div className="content">
                 <h1 className="title">{title}</h1>
                 <span className="subtitle">Shop Now</span>
